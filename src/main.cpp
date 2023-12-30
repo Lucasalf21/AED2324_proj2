52e8c5d277e83fd3dbfe7ef036b34808df39cdb9
Lucas Alfonsi <lucasbranquinhoalf@gmail.com>
2023-12-30 15:05:21 +0000

    data is now loaded in a separate function in main

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,19 @@
 #include "Filereader.h"
 #include "GraphLoader.h"
 
-int main() {
-    cout << "Welcome to the Flight Management System!" << endl;
-    Graph* g = new Graph();
-    Data* data = new Data();
+void loadData(Data* data, Graph* g){
     auto* filereader = new Filereader(data);
     filereader->read();
     GraphLoader graphLoader;
     graphLoader.loadAirports(g, data);
     graphLoader.loadFlights(g, data);
+}
+
+int main() {
+    cout << "Welcome to the Flight Management System!" << endl;
+    Graph* g = new Graph();
+    Data* data = new Data();
+    loadData(data, g);
     Menu menu = Menu(data, g);
     return 0;
 }
